4c4ac9b9a7a72825c0473fdbfd30eac915e57b4e
Philipp Chapkovski <chapkovski@gmail.com>
2019-12-14 21:05:55 -0500

    iconbutton

--- src/components/record.jsx
+++ src/components/record.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ClearIcon from "@material-ui/icons/Clear";
+import IconButton from "@material-ui/core/IconButton";
 const ChatRecord = ({ text, id, deletion }) => {
   console.log(deletion);
   return (
     <ListItem>
       <ListItemText primary={text} />
-      <ClearIcon onClick={()=>deletion(id)} />
+      <IconButton color="secondary" aria-label="clear" component="span">
+        <ClearIcon onClick={() => deletion(id)} />
+      </IconButton>
     </ListItem>
   );
 };
